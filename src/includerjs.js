8305fac5f53855b0dcf3e09744984c89ad87e4de
revdavethompson <davidandrewthompson@gmail.com>
2023-03-17 10:19:34 -0700

    ignore whitespace before includes

--- src/includerjs.js
+++ src/includerjs.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const includeRegex = /^@include\s+(.*)$/;
+const includeRegex = /^\s*@include\s+(.*)$/;
 
 export default function includerjs(input, options = {}) {
     const basePath = options.basePath || '';
